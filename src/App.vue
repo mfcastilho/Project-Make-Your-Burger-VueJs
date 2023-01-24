5544b2fe760fb17bf772be849ef839d9f0def2e5
Mario Frederico Castilho <fredericocastilho@hotmail.com>
2023-01-24 02:17:25 -0300

    Finalizing header and footer of the project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,45 @@
 <template>
-  <Navbar />
-  <router-view/>
+  <Navbar v-bind:logo="logo_src" v-bind:alt="app_name" />
+  <router-view />
   <Footer />
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
-export default{
-  components:{
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+export default {
+  components: {
     Navbar,
-    Footer
-  }
-}
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Make Your Burger",
+    };
+  },
+};
 </script>
 
 <style>
+
+  *{
+    font-family: Helvetica;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container{
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1{
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
 
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>Footer</p>
+  <div id="footer">
+    <p>Make Your Burger &copy; 2023</p>
+  </div>
 </template>
 
 <script>
@@ -11,5 +13,15 @@ export default{
 </script>
 
 <style scoped>
-
+  #footer{
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #FCBA03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+  }
 </style>
